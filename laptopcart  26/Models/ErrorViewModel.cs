4d3f00fd7be00ed329a705a56b586acb35bf7e14
Your Name <your@email.com>
2026-09-23 09:30:03 +0530

    Add project files.

---
+++ laptopcart  26/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace laptopcart__26.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
